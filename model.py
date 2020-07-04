2a219afbd76091b1655a019c0d999952b3739a82
Timen van Gelderen <tvgelderen@outlook.com>
2020-07-04 12:03:24 +0200

    added forward prop to some extent

--- model.py
+++ model.py
@@ -6,8 +6,16 @@ np.random.seed(seed=None)
 
 
 class Model:
+    def __init__(self):
+        self.layers = np.empty(0)
+
     def add(self, layer):
-        self.layer = layer
+        self.layers = np.append(self.layers, layer)
+
+    def propagate_forward(self, input):
+        for layer in self.layers:
+            input = layer.forward(input)
+        print(input)
 
 
 class DenseLayer:
@@ -19,17 +27,27 @@ class DenseLayer:
         self.output = np.zeros(n_output)
 
     def forward(self, input):
-        self.output = np.dot(input, self.weights) + self.biases
+        self.output = self.biases
         if self.activation_function == "relu":
-            self.output = tf.relu(self.output)
+            self.output = tf.nn.relu(self.output)
         elif self.activation_function == "sigmoid":
-            self.output = tf.sigmoid(self.output)
+            self.output = tf.nn.sigmoid(self.output)
         else:
             print("Invalid activation function.")
 
 
+(train_x, train_y), (test_x, test_y) = tf.keras.datasets.mnist.load_data()
+# Change the type to float so as to make sure we get decimal values when normalizing
+train_x, test_x = train_x.astype('float32'), test_x.astype('float32')
+# Normalizing by dividing by the max RGB value
+train_x, test_x = train_x/255, test_x/255
+# Transpose the arrays so they can be input to the input nodes
+train_x, test_x = train_x.transpose(0, 1, 2).reshape(-1, 784), test_x.transpose(0, 1, 2).reshape(-1, 784)
+
 model = Model()
 model.add(DenseLayer(784, 16))
-print(model.layer.weights.shape)
+model.add(DenseLayer(16, 16))
+
+model.propagate_forward(train_x[0])
 
 print("End of file")
